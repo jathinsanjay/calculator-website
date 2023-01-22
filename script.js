b5eac03739139a036cf9333031265906c4af7723
jathinsanjay <bs5418@srmist.edu.in>
2023-01-22 23:06:15 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue = '';
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log('Button text is ', buttonText);
+        if (buttonText == 'Del') {
+  
+            screenValue =screen.value--;
+            screen.value = screenValue;
+        }
+        else if (buttonText == 'C') {
+            screenValue = "";
+            screen.value = screenValue;
+
+        }
+        else if (buttonText == 'Rem') {
+            screenValue+=buttonText;
+                screen.value = buttonText;}
+
+        else if (buttonText == '=') {
+            screen.value = eval(screenValue);
+        }
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+    })
+}
+
